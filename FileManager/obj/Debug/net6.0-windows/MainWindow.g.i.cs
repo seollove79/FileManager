af13b0c56352f08e03557b310f921d2928803020
seollove79 <seollove@gmail.com>
2022-12-16 15:14:14 +0900

    소프트웨어 명칭 변경

--- FileManager/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ FileManager/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -141,7 +141,7 @@ namespace FileManager {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/FileManager;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/FileManager;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
